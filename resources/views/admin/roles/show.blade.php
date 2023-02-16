d1c0bab221d886ab56ee01239d0e7ada1d7bd285
Josue <A281876@alumnos.uaslp.mx>
2023-02-15 21:48:34 -0600

    Proyecto funcional

---
+++ resources/views/admin/roles/show.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'EWAP Panel administrador')
+
+@section('content_header')
+    <h1>EWAP Panel Administrador</h1>
+    <p>Mostrar rol</p>
+@stop
+
+@section('content')
+    <p>Bienvenido al panel de administracion de EWAP </p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
