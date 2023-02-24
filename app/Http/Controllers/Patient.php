e67a01a5c9a83c5b917037e8fe180a5e9e0b7377
[DEadMan10sds] <54693096+DEadMan10sds@users.noreply.github.com>
2023-02-24 11:10:19 -0600

    ADD: EditPatient ADD: Delete patient IMPROVED: addPatient & getPatient

--- app/Http/Controllers/Patient.php
+++ app/Http/Controllers/Patient.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\patient as PatientModel;
 
 class Patient extends Controller
 {
     // public function addPatient(Request $request){
-        
+
         // $patient = new Patient();
         // $patient->name = $request->name;
         // $patient->age = $request->age;
@@ -18,9 +19,28 @@ class Patient extends Controller
     //     return redirect()->back();
     // }
 
-    public function getpatient($id){
-        $patient = Patient::find($id);
-        
-        return view('patient.info');
+    public function addPatient(Request $request) {
+        $newPatient = PatientModel::create($request->patient);
+        if(!$newPatient) {
+            return view("error");
+        }
+        return view("patients.all");
     }
+
+    public function getpatient(Patient $id){
+
+
+        return view('patient.info').with($id);
+    }
+
+    public function editPatient(Request $request, Patient $id){
+        $id->update($request->patientData);
+        dd($id);
+    }
+
+    public function deletePatient(Patient $id){
+        $id->delete();
+        dd($id);
+    }
+
 }
