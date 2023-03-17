53e30da1972af3c5ecfb3a06de976318625be532
Josue <A281876@alumnos.uaslp.mx>
2023-03-17 14:53:54 -0600

    changes in package  - lock

--- Documentos/transport/routes/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\HomeController;
-
-Route::get('', [HomeController::class, 'index']);
-Route::get('', [HomeController::class, 'settings']);
-Route::get('', [HomeController::class, 'tyc']);
