11ec9b70f903f471a6c37345b50792c30f5170e5
Josue <A281876@alumnos.uaslp.mx>
2023-03-21 20:57:10 -0600

    eliminacion de gtignore boostrap

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,5 @@
+<?php return array (
+  'admin.posts-index' => 'App\\Http\\Livewire\\Admin\\PostsIndex',
+  'admin.users-index' => 'App\\Http\\Livewire\\Admin\\UsersIndex',
+  'navigation' => 'App\\Http\\Livewire\\Navigation',
+);
